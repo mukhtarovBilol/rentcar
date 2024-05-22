1895a0012cad5587d5438a23f41f85cdfa8940bb
Bilol <Bilol@gmail.com>
2024-05-22 10:30:01 +0500

    add

--- cars/main.js
+++ cars/main.js
@@ -33,10 +33,6 @@ const getcars = document.querySelectorAll(".getcars");
 let CommonPrice = Number(morePrice?.innerHTML)
 let allPrice = Number(morePrice?.innerHTML)
 var s = 0
-// checkbox2?.addEventListener("click", function () {
-//     checkbox2.classList.toggle("ac8ive")
-// })
-
 
 // date
 
@@ -80,6 +76,7 @@ function calculateRentalCost(days) {
         prices = 90 * days
         morePrice.innerHTML = CommonPrice += prices;
         CommonPrice -= s
+        CommonPrice -= prices
         prices = 0
     } else if (days >= 6 && days <= 13) {
         console.log(days);
@@ -87,21 +84,21 @@ function calculateRentalCost(days) {
         prices = 80 * days 
         morePrice.innerHTML = CommonPrice += prices;
         CommonPrice -= s
-        prices = 0
+        CommonPrice -= prices
     } else if (days >= 14 && days <= 29) {
         console.log(days);
         CommonPrice += s
         prices = 70 * days
         morePrice.innerHTML = CommonPrice += prices;
-        prices = 0
         CommonPrice -= s
+        CommonPrice -= prices
     } else {
         console.log(days);
         CommonPrice += s
         prices = 60 * days
         morePrice.innerHTML = CommonPrice += prices;
-        prices = 0
         CommonPrice -= s
+        CommonPrice -= prices
     }
 }
 
@@ -120,70 +117,6 @@ document.getElementById("start_date").addEventListener("change", function() {
 
 // При изменении даты возврата автоматически пересчитываем стоимость аренды
 document.getElementById("end_date").addEventListener("change", calculate);
-
-
-// // Устанавливаем минимальную дату получения на сегодняшний день
-// var today = new Date();
-// var dd = String(today.getDate()).padStart(2, '0');
-// var mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
-// var yyyy = today.getFullYear();
-// today = yyyy + '-' + mm + '-' + dd;
-// document.getElementById("start_date").setAttribute("min", today);
-
-// // Добавляем обработчик события при изменении даты получения
-// document.getElementById("start_date").addEventListener("change", function() {
-//     calculate(); // Вызываем функцию calculate при изменении даты получения
-// });
-
-// // Добавляем обработчик события при изменении даты возврата
-// document.getElementById("end_date").addEventListener("change", function() {
-//     calculate(); // Вызываем функцию calculate при изменении даты возврата
-// });
-
-// function calculate() {
-//     var startDate = document.getElementById("start_date").value;
-//     var endDate = document.getElementById("end_date").value;
-    
-//     // Проверяем, выбраны ли обе даты
-//     if (startDate && endDate) {
-//         startDate = new Date(startDate);
-//         endDate = new Date(endDate);
-        
-//         // Проверяем, если дата получения позже даты возврата, меняем их местами
-//         if (startDate > endDate) {
-//             var temp = startDate;
-//             startDate = endDate;
-//             endDate = temp;
-//             document.getElementById("start_date").value = startDate.toISOString().slice(0, 10);
-//             document.getElementById("end_date").value = endDate.toISOString().slice(0, 10);
-//         }
-        
-//         // Добавляем еще один день к дате получения
-//         startDate.setDate(startDate.getDate() + 1);
-        
-//         var differenceInTime = endDate.getTime() - startDate.getTime();
-//         var differenceInDays = differenceInTime / (1000 * 3600 * 24) + 1;
-
-//         if (differenceInDays >= 0 && differenceInDays <= 2) {
-//             rentalCost = differenceInDays * 100
-//             CommonPrice += s
-//             morePrice.innerHTML = CommonPrice += rentalCost
-//             CommonPrice -= rentalCost
-//         }else if (differenceInDays >= 2 && differenceInDays <= 5) {
-//                 rentalCost = differenceInDays * 90
-//                 morePrice.innerHTML = CommonPrice + rentalCost;
-//         }else if (differenceInDays >= 5 && differenceInDays <= 13) {
-//                 rentalCost = differenceInDays * 80
-//                 morePrice.innerHTML = CommonPrice + rentalCost;
-//         }else if (differenceInDays >= 13 && differenceInDays <= 29) {
-//             rentalCost = differenceInDays * 70
-//                 morePrice.innerHTML = CommonPrice + rentalCost;
-//         }else if (differenceInDays >= 29) {
-//             rentalCost = differenceInDays * 60
-//             morePrice.innerHTML = CommonPrice += rentalCost;
-//         }
-//     }
-// }
 
 // date
 
@@ -226,59 +159,27 @@ headerSelectValue7.addEventListener("change", function () {
 //     }
 // })
 
-// for (let i = 0; i < moreInfoPrice.length; i++) {
-    checkbox.addEventListener("click", function () {
-        checkbox.classList.toggle("active")
-        // sit.classList.toggle('active')
-        // sit?.classList.remove("active")
-        // if (checkbox[0]?.classList == 'checkbox active') {
-        // }else {
-        // }
-
-        // if (checkbox[1]?.classList == 'checkbox active') {
-        //     animals.classList.add("active")
-        // }else {
-        //     animals?.classList.remove("active")
-        // }
-        
-        // if (checkbox[2]?.classList == 'checkbox active') {
-        //     drive?.classList.add("active")
-        // }else {
-        //     drive?.classList.remove("active")
-        // }
-        // if (checkbox?.classList == 'checkbox active') {
-        //     let price = Number(moreInfoPrice.getAttribute("data-src"))
-        //     // console.log(moreInfoPrice[i].getAttribute("data-src"));
-        //     CommonPrice = CommonPrice + price
-        //     morePrice.innerHTML = CommonPrice
-        //     // console.log(a);
-        // }else {
-        //     let price = Number(moreInfoPrice.getAttribute("data-src"))
-        //     CommonPrice = CommonPrice - price
-        //     morePrice.innerHTML = CommonPrice
-        // }
-        if (checkbox?.classList == 'checkbox active') {
-            let price = Number(moreInfoPrice.getAttribute("data-src"))
-            // console.log(moreInfoPrice[i].getAttribute("data-src"));
-            CommonPrice += s
-            CommonPrice += prices
-            morePrice.innerHTML = CommonPrice += price
-            CommonPrice -= s
-            CommonPrice -= prices
-            allPrice = CommonPrice
-        }else{
-            CommonPrice += s
-            CommonPrice += prices
-            let price = Number(moreInfoPrice.getAttribute("data-src"))
-            morePrice.innerHTML = CommonPrice -= price 
-            CommonPrice -= s
-            CommonPrice -= prices
-            allPrice = CommonPrice  
-        }
-    })
-// }
-
-
+checkbox.addEventListener("click", function () {
+    checkbox.classList.toggle("active")
+    if (checkbox?.classList == 'checkbox active') {
+        let price = Number(moreInfoPrice.getAttribute("data-src"))
+        // console.log(moreInfoPrice[i].getAttribute("data-src"));
+        CommonPrice += s
+        CommonPrice += prices
+        morePrice.innerHTML = CommonPrice += price
+        CommonPrice -= s
+        CommonPrice -= prices
+        allPrice = CommonPrice
+    }else{
+        CommonPrice += s
+        CommonPrice += prices
+        let price = Number(moreInfoPrice.getAttribute("data-src"))
+        morePrice.innerHTML = CommonPrice -= price 
+        CommonPrice -= s
+        CommonPrice -= prices
+        allPrice = CommonPrice  
+    }
+})
 
 // check
 
@@ -291,14 +192,7 @@ const select2 = document.querySelector(".select2");
 const select3 = document.querySelector(".select3");
 const select4 = document.querySelector(".select4");
 const headerInfoSelect = document.querySelector(".header__info-select");
-// const headerInfoSelectAirport = document.querySelector(".header__info-selectAirport")
-// const headerSelectValue3 = document.querySelector(".header__info-selectValue3");
-// const headerSelectValue4 = document.querySelector(".header__info-selectValue4");
-// const headerSelectValue5 = document.querySelector(".header__info-selectValue5");
-// const headerSelectValue6 = document.querySelector(".header__info-selectValue6");
 const headerInfoCount = document.querySelector(".header__info-count")
-
-
 
 // count passanger
 var countPassanger = ''
@@ -323,7 +217,6 @@ headerInfoInput2.addEventListener("change", function () {
     // console.log(headerInfoInput2.value);
     comeback = headerInfoInput2.value
 })
-
 
 // дата возврата
 
@@ -360,10 +253,6 @@ select4.addEventListener("change", function () {
 })
 // минута 
 
-  // Открыть модальное окно
-// document.getElementById("open-modal-btn").addEventListener("click", function() {
-//     document.getElementById("my-modal").classList.add("open")
-// })
 
 const headerLink = document.querySelector(".header__info-link")
 
@@ -404,28 +293,10 @@ function sendMail(){
             childrenSitting = "Нет"
         }
 
-        // if (animals?.classList == 'more__info animals active') {
-        //     var animal = "Да"
-        // }else {
-        //     animal = "Нет"
-        // }
-
-        // if (drive?.classList == 'more__info drive active') {
-        //     var driven = "Да"
-        // }else {
-        //     driven = "Нет"
-        // }
-
-        // if (checkbox2?.classList == "checkbox2 active") {
-        //     var returned = "Да"
-        // } else {
-        //     returned = "Нет"
-        // }
-
         var params = {
             senderName: document.querySelector("#name").value,
             name: document.querySelector("#name").value + ", Дата и время получение машины: " + getcar + ' ' + time + ':' + minute + ', Дата и время возврата: ' + comeback + ' ' + backtime + ' : ' + backminute + ', Возврат авто в другом месте: ' + returned, 
-            message: document.querySelector("#message").value + ', Нужен ли кресла: ' + childrenSitting + ', Место получение: ' + getsCars + ', Место получение авто: ' + getPlace + ' ' + getPlaceAdress
+            message: document.querySelector("#message").value + ', Нужен ли кресла: ' + childrenSitting + ', Место получение: ' + getsCars + ', Место получение авто: ' + getPlace 
         };
 
 
