cdc89d9b5dae31ec322a489b7bfa343b4c21687b
Bilol <Bilol@gmail.com>
2024-05-22 20:07:01 +0500

    add new function

--- cars/main.js
+++ cars/main.js
@@ -3,17 +3,17 @@ var swiper = new Swiper(".mySwiper", {
     slidesPerView: 4,
     freeMode: true,
     watchSlidesProgress: true,
-  });
-  var swiper2 = new Swiper(".mySwiper2", {
+});
+var swiper2 = new Swiper(".mySwiper2", {
     spaceBetween: 10,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
     thumbs: {
-      swiper: swiper,
+        swiper: swiper,
     },
-  });
+});
 
 // check
 
@@ -30,6 +30,11 @@ const headerSelectValue7 = document.querySelector(".header__info-selectValue7");
 const headerSelectValue8 = document.querySelector(".header__info-selectValue8");
 const getcars = document.querySelectorAll(".getcars");
 const getcars2 = document.querySelectorAll(".getcars2");
+const number1 = document.querySelector(".number1")
+const number2 = document.querySelector(".number2")
+const number3 = document.querySelector(".number3")
+const number4 = document.querySelector(".number4")
+const number5 = document.querySelector(".number5")
 
 let CommonPrice = Number(morePrice?.innerHTML)
 let allPrice = Number(morePrice?.innerHTML)
@@ -41,64 +46,80 @@ var s2 = 0 // это место получение офис и т.д(2)
 function calculate() {
     var startDate = new Date(document.getElementById("start_date").value);
     var endDate = new Date(document.getElementById("end_date").value);
-    
+
     // Проверяем, если дата возврата не выбрана, прерываем функцию
     if (!endDate) return;
-    
+
     // Проверяем, если дата получения позже даты возврата, выводим сообщение об ошибке
     if (startDate >= endDate) {
         alert("Пожалуйста, выберите корректные даты для получения и возврата.");
         return;
     }
-    
+
     var differenceInTime = endDate.getTime() - startDate.getTime();
     var differenceInDays = Math.ceil(differenceInTime / (1000 * 3600 * 24)); // округляем вверх, чтобы учитывать день возврата
-    
+
     if (differenceInDays < 1) {
         alert("Минимальный срок аренды составляет 1 день.");
         return;
     }
-    
+
     var rentalCost = calculateRentalCost(differenceInDays);
 }
 
 
 var prices = 0;
+var innerNumber1 = Number(number1?.innerHTML) 
+var innerNumber2 = Number(number2?.innerHTML) 
+var innerNumber3 = Number(number3?.innerHTML) 
+var innerNumber4 = Number(number4?.innerHTML) 
+var innerNumber5 = Number(number5?.innerHTML) 
+
 function calculateRentalCost(days) {
     if (days >= 1 && days <= 2) {
-        console.log(days);
-        prices = 100 * days
+        // console.log(days);
+        prices = innerNumber1 * days
         CommonPrice += s
-        morePrice.innerHTML = CommonPrice += prices;
+        if (morePrice) {
+            morePrice.innerHTML = CommonPrice += prices;
+        }
         CommonPrice -= s
         CommonPrice -= prices
     } else if (days >= 3 && days <= 5) {
-        console.log(days);
+        // console.log(days);
         CommonPrice += s
-        prices = 90 * days
-        morePrice.innerHTML = CommonPrice += prices;
+        prices = innerNumber2 * days
+        if (morePrice) {
+            morePrice.innerHTML = CommonPrice += prices;
+        }
         CommonPrice -= s
         CommonPrice -= prices
         prices = 0
     } else if (days >= 6 && days <= 13) {
-        console.log(days);
+        // console.log(days);
         CommonPrice += s
-        prices = 80 * days 
-        morePrice.innerHTML = CommonPrice += prices;
+        prices = innerNumber3 * days
+        if (morePrice) {
+            morePrice.innerHTML = CommonPrice += prices;
+        }
         CommonPrice -= s
         CommonPrice -= prices
     } else if (days >= 14 && days <= 29) {
-        console.log(days);
+        // console.log(days);
         CommonPrice += s
-        prices = 70 * days
-        morePrice.innerHTML = CommonPrice += prices;
+        prices = innerNumber4 * days
+        if (morePrice) {
+            morePrice.innerHTML = CommonPrice += prices;
+        }
         CommonPrice -= s
         CommonPrice -= prices
     } else {
-        console.log(days);
+        // console.log(days);
         CommonPrice += s
-        prices = 60 * days
-        morePrice.innerHTML = CommonPrice += prices;
+        prices = innerNumber5 * days
+        if (morePrice) {
+            morePrice.innerHTML = CommonPrice += prices;
+        }
         CommonPrice -= s
         CommonPrice -= prices
     }
@@ -109,7 +130,7 @@ function calculateRentalCost(days) {
 document.getElementById("start_date").min = new Date().toISOString().split('T')[0];
 
 // При изменении даты получения автоматически обновляем минимальную дату возврата
-document.getElementById("start_date").addEventListener("change", function() {
+document.getElementById("start_date").addEventListener("change", function () {
     var startDate = new Date(document.getElementById("start_date").value);
     var nextDay = new Date(startDate);
     nextDay.setDate(startDate.getDate() + 1);
@@ -133,7 +154,7 @@ headerSelectValue7.addEventListener("change", function () {
         CommonPrice -= s
         CommonPrice -= s2
         CommonPrice -= prices
-    }else if (headerSelectValue7.value == 'airport2') {
+    } else if (headerSelectValue7.value == 'airport2') {
         s = 25
         CommonPrice += prices
         CommonPrice += s2
@@ -141,7 +162,7 @@ headerSelectValue7.addEventListener("change", function () {
         CommonPrice -= s
         CommonPrice -= s2
         CommonPrice -= prices
-    }else {
+    } else {
         s = 0
         CommonPrice += prices
         CommonPrice += s2
@@ -163,15 +184,15 @@ headerSelectValue8.addEventListener("change", function () {
         CommonPrice -= 10
         CommonPrice -= s
         CommonPrice -= prices
-    }else if (headerSelectValue8.value == 'airaport2') {
+    } else if (headerSelectValue8.value == 'airaport2') {
         s2 = 25
         CommonPrice += prices
         CommonPrice += s
         morePrice.innerHTML = CommonPrice += 25
-        CommonPrice -= 25 
+        CommonPrice -= 25
         CommonPrice -= s
         CommonPrice -= prices
-    }else {
+    } else {
         s2 = 0
         CommonPrice += prices
         CommonPrice += s
@@ -181,11 +202,10 @@ headerSelectValue8.addEventListener("change", function () {
     }
 })
 
-checkbox.addEventListener("click", function () {
+checkbox?.addEventListener("click", function () {
     checkbox.classList.toggle("active")
     if (checkbox?.classList == 'checkbox active') {
-        let price = Number(moreInfoPrice.getAttribute("data-src"))
-        // console.log(moreInfoPrice[i].getAttribute("data-src"));
+        let price = Number(moreInfoPrice.innerHTML)
         CommonPrice += s
         CommonPrice += prices
         CommonPrice += s2
@@ -194,16 +214,16 @@ checkbox.addEventListener("click", function () {
         CommonPrice -= s2
         CommonPrice -= prices
         allPrice = CommonPrice
-    }else{
+    } else {
         CommonPrice += s
         CommonPrice += s2
         CommonPrice += prices
-        let price = Number(moreInfoPrice.getAttribute("data-src"))
-        morePrice.innerHTML = CommonPrice -= price 
+        let price = Number(moreInfoPrice.innerHTML)
+        morePrice.innerHTML = CommonPrice -= price
         CommonPrice -= s
         CommonPrice -= s2
         CommonPrice -= prices
-        allPrice = CommonPrice  
+        allPrice = CommonPrice
     }
 })
 
@@ -252,19 +272,19 @@ headerInfoDataSelect.addEventListener("change", function () {
     // console.log(headerInfoDataSelect.value);
     time = headerInfoDataSelect.value
 })
- 
+
 // время получение
 
 // минута получение
 var minute = '00'
 select2.addEventListener("change", function () {
-     minute = select2.value
+    minute = select2.value
     // console.log(select2.value);
 })
 // минута получение
 
 // время 
-var backtime = '' 
+var backtime = ''
 select3.addEventListener("change", function () {
     // console.log(select3.value);
     backtime = select3.value
@@ -287,7 +307,7 @@ headerLink.addEventListener("click", function () {
 })
 
 // Закрыть модальное окно
-document.getElementById("close-my-modal-btn").addEventListener("click", function() {
+document.getElementById("close-my-modal-btn").addEventListener("click", function () {
     document.getElementById("my-modal").classList.remove("open")
 })
 
@@ -307,55 +327,35 @@ document.getElementById("my-modal").addEventListener('click', event => {
     event.currentTarget.classList.remove('open');
 });
 
-function sendMail(){
-        if (document.querySelector("#name").value !== '' && document.querySelector("#message").value !=='') {     
-        (function(){
-            emailjs.init("gaVbQy_bCrOGZXTtf")   
+function sendMail() {
+    if (document.querySelector("#name").value !== '' && document.querySelector("#message").value !== '') {
+        (function () {
+            emailjs.init("gaVbQy_bCrOGZXTtf")
         })();
 
         if (sit?.classList == 'more__info sit active') {
             var childrenSitting = "Да"
-        }else {
+        } else {
             childrenSitting = "Нет"
         }
 
         var params = {
             senderName: document.querySelector("#name").value,
-            name: document.querySelector("#name").value + ", Дата и время получение машины: " + getcar + ' ' + time + ':' + minute + ', Дата и время возврата: ' + comeback + ' ' + backtime + ' : ' + backminute + ', Возврат авто в другом месте: ' + returned, 
-            message: document.querySelector("#message").value + ', Нужен ли кресла: ' + childrenSitting + ', Место получение: ' + getsCars + ', Место получение авто: ' + getPlace 
+            name: document.querySelector("#name").value + ", Дата и время получение машины: " + getcar + ' ' + time + ':' + minute + ', Дата и время возврата: ' + comeback + ' ' + backtime + ' : ' + backminute,
+            message: document.querySelector("#message").value + ', Нужен ли кресла: ' + childrenSitting + ', Место получение машины: ' + getsCars + ', Место возврата авто: ' + backCars + ", Итого: " + morePrice.innerHTML
         };
 
 
         var serviceID = "service_1xyqmdu";
         var templateID = "template_tgol57n";
-    
+
         emailjs.send(serviceID, templateID, params)
-        .then( res => {
-            alert("Доставлено, скоро с вами свяжутся")
-            document.querySelector("#name").value = ''
-            document.querySelector("#message").value = ''
-            document.querySelector("#date").value = ''
-        })
+            .then(res => {
+                alert("Доставлено, скоро с вами свяжутся")
+                document.querySelector("#name").value = ''
+                document.querySelector("#message").value = ''
+                document.querySelector("#date").value = ''
+            })
         .catch();
     }
 }
-
-// date
-var date = document.getElementById("date")
-
-var a = new Date().getFullYear() 
-if (new Date().getMonth()+1 < 10) {
-    var b = '0' + (new Date().getMonth()+1)
-}else {
-    b = new Date().getMonth()+1
-}
-if (new Date().getDate() < 10) {
-    var c = '0' + new Date().getDate()
-}else {
-    c = new Date().getDate()
-}
-
-var fullYear = a + '-' + b + '-' + c
-// document.write(a)
-
-date.min = fullYear
